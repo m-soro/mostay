a5a644248c41114e23a73e5641f27554aab2eeb6
mark-soro <napoleon_soro@rocketmail.com>
2022-05-01 14:08:24 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# MO Stay
+
+![image](files/logo4.png)
+
+Get MO Stay availability up to the next two months.
+
+![image](files/demo.gif)
+
+[View the source code](https://github.com/m-soro/mostay/blob/main/mostay)
+
+[Visit Mandarin Oriental Website](https://www.mandarinoriental.com/)
---
+++ mandarin_locations.py
@@ -0,0 +1,9 @@
+mo_dict = {
+    "Bangkok":"510","Barcelona":"23895","BeijingWangfujing":"2265","AbuDhabi":"10921",
+    "Bodrum":"61547","Boston":"21547","Canouan":"79725","Doha":"71855","Dubai":"2979",
+    "Geneva":"523","Guangzhou":"56732","HongKongMandarinOriental":"514","HongKongTheLandmark":"556",
+    "Istanbul":"31761","Jakarta":"528","KualaLumpur":"516","LakeComo":"5078",
+    "London":"508","Macau":"28043","Madrid":"65443","Marrakech":"63639","Miami":"526",
+    "Milan":"63638","Munich":"525","NewYork":"532","Paris":"52453","Prague":"639",
+    "Riyadh":"30797","Santiago":"78149","Sanya":"648","Shanghai":"58160","Shenzhen":"36052",
+    "Singapore":"509","Taipei":"59555","Tokyo":"558","WashingtonDC":"535" }
